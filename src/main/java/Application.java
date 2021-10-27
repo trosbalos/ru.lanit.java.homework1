987849b82bdf961a685de04056b79c049211ae8d
trosbalos <62149688+trosbalos@users.noreply.github.com>
2021-10-27 19:33:08 +0300

    Home work 1

---
+++ src/main/java/Application.java
@@ -0,0 +1,21 @@
+import model.Kotik;
+
+public class Application {
+    /*
+    Создать в корневом каталоге с программой класс Application
+    с точкой входа в программу (метод public static void main(String[] args)).
+     */
+    public static void main(String[] args) {
+        Kotik murizik = new Kotik(0,3,"Murzik",4,"уоууу");
+        Kotik barsik = new Kotik();
+        barsik.setKotik(0,3,"Barsik",4,"уоууу");
+        murizik.liveAnotherDay();
+
+        System.out.println(barsik.getName());
+        System.out.println(barsik.getWeight());
+
+        System.out.println(barsik.getMeo().equals(murizik.getMeo()));
+
+        System.out.println(Kotik.getCatsCount());
+    }
+}
---
+++ src/main/java/model/Kotik.java
@@ -0,0 +1,158 @@
+package model;
+
+public class Kotik {
+
+    private int food = 0;
+    private int prettiness;
+    private String name;
+    private int weight;
+    private String meo;
+
+
+
+    private static int catsCount = 0;
+
+
+    public boolean play() {
+
+        if (food > 0) {
+            System.out.println("поиграл");
+            food--;
+        }
+        return true;
+    }
+
+    public boolean sleep() {
+
+        if (food > 0) {
+            System.out.println("поспал");
+            food--;
+        }
+        return true;
+    }
+
+    public boolean meow() {
+
+        if (food > 0) {
+            System.out.println("мяукнул");
+            food--;
+        }
+        return true;
+    }
+
+    public boolean eat(int food) {
+        this.food = food;
+        return true;
+    }
+
+    public boolean eat(int x, String foodName) {
+        this.food += x;
+        System.out.println("поел " + x + " " + foodName);
+        return true;
+    }
+
+    public boolean eat() {
+        eat(4, "колбаски");
+        return true;
+    }
+
+    public boolean isHungry() {
+
+        if (food <= 0) {
+            System.out.println("голодный");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean jump() {
+
+        if (food > 0) {
+            System.out.println("прыгнул");
+            food--;
+        }
+        return true;
+    }
+
+    public boolean run() {
+
+        if (food > 0) {
+            System.out.println("побегал");
+            food--;
+        }
+        return true;
+    }
+
+    public Kotik() {
+        catsCount++;
+    }
+
+    public Kotik(int food, int prettiness, String name, int weight, String meo) {
+        this.food = food;
+        this.prettiness = prettiness;
+        this.name = name;
+        this.weight = weight;
+        this.meo = meo;
+        catsCount++;
+    }
+
+    public void setKotik(int food, int prettiness, String name, int weight, String meo) {
+        this.food = food;
+        this.prettiness = prettiness;
+        this.name = name;
+        this.weight = weight;
+        this.meo = meo;
+
+    }
+
+    public void liveAnotherDay() {
+        for (int i = 0; i < 24; i++) {
+            if (isHungry()) {
+                int n = (int) (Math.random() * 5);
+                switch (n) {
+                    case 1:
+                        play();
+                        break;
+                    case 2:
+                        sleep();
+                        break;
+                    case 3:
+                        jump();
+                        break;
+                    case 4:
+                        meow();
+                        break;
+                    case 5:
+                        run();
+                        break;
+
+                }
+            }
+            else eat(5, "сосисок");
+        }
+
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public int getPrettiness() {
+        return prettiness;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getMeo() {
+        return meo;
+    }
+    public static int getCatsCount() {
+        return catsCount;
+    }
+}
